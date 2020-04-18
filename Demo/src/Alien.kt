3b66a75e1995a5a32882dc2b299c4af184bd46cd
punyashlokam <punyam@menlo-technologies.com>
2020-04-18 22:37:50 +0530

    val vs var

---
+++ Demo/src/Alien.kt
@@ -0,0 +1,9 @@
+
+class Alien {
+var name : String = "XOXO"
+val birth_name : String = "XOXO" //constant as final in java
+
+
+
+
+}
--- Demo/src/FirstCode.kt
+++ Demo/src/FirstCode.kt
@@ -2,4 +2,9 @@
 
 fun main(args : Array<String>){
     println("Hello Aliens")
+    var alien = Alien();
+    println("alien birth name is : " + alien.birth_name)
+    alien.name = "YOYO"
+    println("alien name is changed in earth to : ${alien.name}")
+
 }
