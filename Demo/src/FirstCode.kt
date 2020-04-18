a3d319dc0db8c8967cc3a8e1ff5fd50aaae62404
punyashlokam <punyam@menlo-technologies.com>
2020-04-18 22:43:51 +0530

    java code in kotlin file

--- Demo/src/FirstCode.kt
+++ Demo/src/FirstCode.kt
@@ -2,9 +2,16 @@
 
 fun main(args : Array<String>){
     println("Hello Aliens")
-    var alien = Alien();
+    var alien = Alien()
     println("alien birth name is : " + alien.birth_name)
     alien.name = "YOYO"
     println("alien name is changed in earth to : ${alien.name}")
 
+    //create object of java class
+    var telusko = JAlien()
+    telusko.birthName = "telusko"
+    telusko.name = "JAADOO"
+
+    println("alien birth name is ${telusko.birthName}")
+    println("alien name is changed in earth to : ${telusko.name}")
 }
---
+++ Demo/src/JAlien.java
@@ -0,0 +1,21 @@
+public class JAlien {
+    private String name;
+    private String birthName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthName() {
+        return birthName;
+    }
+
+    public void setBirthName(String birthName) {
+        this.birthName = birthName;
+    }
+
+}
